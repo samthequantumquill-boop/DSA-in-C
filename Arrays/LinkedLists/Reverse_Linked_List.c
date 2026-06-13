bc493722f9dbb853a895f38a092338065ec23711
Samriddhi Singh <samthequantumquill@gmail.com>
2026-06-13 13:10:05 +0530

    Implement function to reverse a linked list

---
+++ Arrays/LinkedLists/Reverse_Linked_List.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode *prev = NULL;
+    struct ListNode *cur = head;
+    struct ListNode *next;
+    while(cur != NULL){
+        next = cur->next;
+        cur ->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
